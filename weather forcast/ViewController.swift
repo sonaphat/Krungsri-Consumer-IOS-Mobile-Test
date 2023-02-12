909ec023d4faea26f1a61546af31700e115e4565
Naphat Sottiyaphai <naphat_s@apthai.com>
2023-02-12 21:50:02 +0700

    init project xcode

---
+++ weather forcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather forcast
+//
+//  Created by Naphat Sottiyaphai on 12/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
